f4475d78a3215bcca0165f27c17d4d8a14d5b2e5
alexiles06 <rivascf@gmail.com>
2025-12-01 01:06:19 -0600

    Practicas 1 y 2

---
+++ scara_robot_ws/build/scara_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arrgusr/ROS2Dev/scara_robot_ws/src/scara_description
+# Build directory: /home/arrgusr/ROS2Dev/scara_robot_ws/build/scara_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
