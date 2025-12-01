ff96753148b3fc275b84139588b25b08f985081e
Alejandro Islas Escobar <153693213+alexisles06@users.noreply.github.com>
2025-12-01 00:58:32 -0600

    Delete build directory

--- build/scara_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arrgusr/ROS2Dev/scara_robot_ws/src/scara_bringup
-# Build directory: /home/arrgusr/ROS2Dev/scara_robot_ws/build/scara_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/scara_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arrgusr/ROS2Dev/scara_robot_ws/src/scara_description
-# Build directory: /home/arrgusr/ROS2Dev/scara_robot_ws/build/scara_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
